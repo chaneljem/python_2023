ca70a2cf70edee0e1f44d680c1a70c40d861ab1e
chaneljem <chanel.jemmott@gmail.com>
2023-02-07 01:47:21 +0000

    lists

---
+++ lists.py
@@ -0,0 +1,21 @@
+#Lists
+var = []
+print(var)
+print(type(var))
+
+var2 = [151, 251, 386, "009"]
+print(var2)
+
+numbers = [0, 1, 2, 3, 4]
+print(numbers)
+
+for number in numbers:
+    print(number * 2)
+
+number_list = list(range(2,10))
+print(number_list)
+
+print(number_list[3]) # indexing starts at zero. Should print 5
+
+print(len(number_list))
+
